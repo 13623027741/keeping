f7d17304a1d153272d1a430222ef8d07fc49c461
13623027741 <13623027741@163.com>
2016-06-14 11:08:00 +0800

    初始化项目

---
+++ keeping/Classes/Login/Controller/LoginController.h
@@ -0,0 +1,13 @@
+//
+//  LoginController.h
+//  keeping
+//
+//  Created by kaidan on 16/6/14.
+//  Copyright © 2016年 kaidan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginController : UIViewController
+
+@end
---
+++ keeping/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  keeping
+//
+//  Created by kaidan on 16/6/14.
+//  Copyright © 2016年 kaidan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
